1131c8a8339519c427782d875c828ef07c6460f3
ciscodevelop <ciscopp@gmail.com>
2023-01-11 22:16:10 +0100

    aggiornato App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ViewInfo from "./shared/componet/ViewInfo";
-import MapsInfo from "./shared/componet/MapsInfo";
 
 function App() {
   return (
     <header className="container">
-     
-      <ViewInfo/>
-     
+      <ViewInfo />
     </header>
   );
 }
