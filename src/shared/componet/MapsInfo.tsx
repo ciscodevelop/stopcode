65513bce280d667feff89a37191516bd0327bbcf
ciscodevelop <ciscopp@gmail.com>
2023-01-12 12:25:43 +0100

    aggiunto Markrer al Map

--- src/shared/componet/MapsInfo.tsx
+++ src/shared/componet/MapsInfo.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from "react";
-import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader ,MarkerF} from "@react-google-maps/api";
 
 const MapsInfo = ({lat,long}:any) => {
   const { isLoaded } = useJsApiLoader({
@@ -7,18 +7,18 @@ const MapsInfo = ({lat,long}:any) => {
     googleMapsApiKey: "AIzaSyA1RO6BWdgnZ_0b_N3kuWqxRmWqOXwPxmU",
   });
 
-  if (!isLoaded) return <div>in caricamento</div>;
-  return <Map></Map>;
+  if (!isLoaded) return <div>in caricamento...</div>;
+  return <Map/>;
 
   function Map() {
     const center =useMemo(() => ({lat:lat,lng: long}),[]) ;
     return (
       <GoogleMap
-        zoom={4}
+        zoom={5}
         center={center}
         mapContainerClassName="map-container"
       >
-        <Marker position={center} />
+        <MarkerF position={center} />
       </GoogleMap>
     );
   }
